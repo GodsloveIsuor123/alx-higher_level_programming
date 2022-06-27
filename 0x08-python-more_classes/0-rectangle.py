a7e69e01f18b88e93f898479155c43d5492905ab
root <godsloveisuor@gmail.com>
2022-06-27 09:02:50 -0700

    -more_classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Simple rectangle : An empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty Rectangle class
+    """
+
+    pass
